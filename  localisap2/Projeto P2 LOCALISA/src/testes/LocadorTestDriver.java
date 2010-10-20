31e8425336cc0005d5e1d885c1a93d0f105d5d92
militoufcg <militoufcg@91337772-dbf3-1cee-e463-7e92b6bf6caa>
2010-10-20 22:26:01 +0000

    Testes - verificaCpf nao funfa...

---  localisap2/Projeto P2 LOCALISA/src/testes/LocadorTestDriver.java
+++  localisap2/Projeto P2 LOCALISA/src/testes/LocadorTestDriver.java
@@ -7,6 +7,10 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import agencias.Agencia;
+import agencias.Filial;
+
+import funcionarios.Gerente;
 import funcionarios.Locador;
 
 
@@ -16,30 +20,34 @@ import funcionarios.Locador;
  */
 public class LocadorTestDriver {
 	Locador locador;
+	Agencia filial1, filial2;
+	Gerente gerente;
 	@Before
 	public void inicio() throws Exception{
+		filial1 = new Filial("51851873000149", "Aqui em casa", "8333331234", "01234", gerente);
+		filial2 = new Filial("58694458000178", "Lah no japao", "8300004321", "56789", gerente);
 		locador = new Locador();
-		locador.setAgencia("agencia");
-		locador.setCPF("01234567891");
+		locador.setAgencia(filial1);
+		locador.setCpf("32456373570");
 		locador.setEmail("filipe@teste");
 		locador.setEndereco("endereco");
 		locador.setNascimento("11/10/1980");
 		locador.setNaturalidade("Paraiba");
 		locador.setNome("Filipe");
-		locador.setRG("123456");
+		locador.setRg("123456");
 		locador.setTelefone("8333311111");
 	}
 	
 	@Test
 	public void testaMetodosGet(){
-		Assert.assertEquals("01234567891",locador.getCPF());
-		Assert.assertEquals("agencia",locador.getAgencia());
+		Assert.assertEquals("32456373570",locador.getCpf());
+		Assert.assertEquals(filial1,locador.getAgencia());
 		Assert.assertEquals("filipe@teste",locador.getEmail());
 		Assert.assertEquals("endereco",locador.getEndereco());
 		Assert.assertEquals("11/10/1980",locador.getNascimento());
 		Assert.assertEquals("Paraiba",locador.getNaturalidade());
 		Assert.assertEquals("Filipe",locador.getNome());
-		Assert.assertEquals("123456",locador.getRG());
+		Assert.assertEquals("123456",locador.getRg());
 		Assert.assertEquals("8333311111",locador.getTelefone());
 		
 	}
@@ -68,23 +76,23 @@ public class LocadorTestDriver {
 	*/
 	@Test
 	public void testaMetodosSet() throws Exception{
-		locador.setAgencia("agencia2");
-		locador.setCPF("01234567890");
+		locador.setAgencia(filial2);
+		locador.setCpf("58548316780");
 		locador.setEmail("filipe@teste2");
 		locador.setEndereco("endereco2");
 		locador.setNascimento("10/10/1980");
 		locador.setNaturalidade("Sao Paulo");
 		locador.setNome("Filipe A");
-		locador.setRG("1234567");
+		locador.setRg("1234567");
 		locador.setTelefone("8333312222");
-		Assert.assertEquals("01234567890",locador.getCPF());
-		Assert.assertEquals("agencia2",locador.getAgencia());
+		Assert.assertEquals("58548316780",locador.getCpf());
+		Assert.assertEquals(filial2,locador.getAgencia());
 		Assert.assertEquals("filipe@teste2",locador.getEmail());
 		Assert.assertEquals("endereco2",locador.getEndereco());
 		Assert.assertEquals("10/10/1980",locador.getNascimento());
 		Assert.assertEquals("Sao Paulo",locador.getNaturalidade());
 		Assert.assertEquals("Filipe A",locador.getNome());
-		Assert.assertEquals("1234567",locador.getRG());
+		Assert.assertEquals("1234567",locador.getRg());
 		Assert.assertEquals("8333312222",locador.getTelefone());
 	}
 }
