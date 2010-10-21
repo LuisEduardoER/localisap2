315424cfa396b71f1c74f9bc10ffccff2c4c7293
filipe.69 <filipe.69@91337772-dbf3-1cee-e463-7e92b6bf6caa>
2010-10-21 18:55:14 +0000

    Alteração de planos

---  localisap2/Projeto P2 LOCALISA/src/planos/PlanosAutomovel.java
+++  localisap2/Projeto P2 LOCALISA/src/planos/PlanosAutomovel.java
@@ -4,18 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 import veiculos.Automovel;
+import veiculos.Veiculo.Acessorios;
 
 public class PlanosAutomovel {
 	
-	List<Automovel> listaAuto = new ArrayList<Automovel>();
+	private List<Automovel> listaAuto = new ArrayList<Automovel>();
+	private List<Acessorios> listaAcessorios = new ArrayList<Acessorios>();
+
 	private double preco;
 	
 	public PlanosAutomovel(double preco){
 		this.preco = preco;
 	}
-	
-	public void adicionaCarro(Automovel carro){
+	public void adicionaAcessorios(Acessorios item){
+		listaAcessorios.add(item);
+	}
+	public void adicionaCarro(Automovel carro)throws Exception{
+		int tamanho = listaAcessorios.size();
+		int acertos = 0;
+		for (Acessorios item: carro.getOpcionais()){
+			if(listaAcessorios.contains(item))
+				acertos++;
+		}
+		if(tamanho != acertos)
+			throw new Exception("O carro não atende as especicaçoes do plano");
 		this.listaAuto.add(carro);
+		
 	}
 	
 	public void removeCarro(Automovel carro) throws Exception{
---  localisap2/Projeto P2 LOCALISA/src/planos/PlanosMoto.java
+++  localisap2/Projeto P2 LOCALISA/src/planos/PlanosMoto.java
@@ -1,20 +1,26 @@
 package planos;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import veiculos.Motocicleta;
+import veiculos.Veiculo.Acessorios;
 
 public class PlanosMoto {
 	
 	private List<Motocicleta> listaMoto = new ArrayList<Motocicleta>();
+	private int cilindradas;
 	private double preco;
 	
 	public PlanosMoto(double preco){
 		this.preco = preco;
 	}
+	public void adicionaCilindradas(int cilindradas){
+		this.cilindradas = cilindradas;
+	}
 	
-	public void adicionaMoto(Motocicleta moto){
+	public void adicionaMoto(Motocicleta moto)throws Exception{
+		if (moto.getCilindradas()>cilindradas)
+			throw new Exception("A moto não atende as especicaçoes do plano");
 		this.listaMoto.add(moto);
 	}
 	
