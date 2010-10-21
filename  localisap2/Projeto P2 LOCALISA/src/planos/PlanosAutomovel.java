de8ccdbc99fac8c1f9dfaa75792bd38e1d6c935a
filipe.69 <filipe.69@91337772-dbf3-1cee-e463-7e92b6bf6caa>
2010-10-21 20:56:18 +0000

    Javadoc planos

---  localisap2/Projeto P2 LOCALISA/src/planos/PlanosAutomovel.java
+++  localisap2/Projeto P2 LOCALISA/src/planos/PlanosAutomovel.java
@@ -1,5 +1,4 @@
 package planos;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,15 +9,26 @@ public class PlanosAutomovel {
 	
 	private List<Automovel> listaAuto = new ArrayList<Automovel>();
 	private List<Acessorios> listaAcessorios = new ArrayList<Acessorios>();
-
 	private double preco;
-	
+	/**
+	 * Construtor de um plano de automovel.
+	 * @param preco - Recebe o preco em double diario da locacao
+	 */
 	public PlanosAutomovel(double preco){
 		this.preco = preco;
 	}
+	/**
+	 * Adiciona os acessorios que o plano contempla.
+	 * @param item - Recebe o acessorio a ser adicionado no tipo Acessorios.
+	 */
 	public void adicionaAcessorios(Acessorios item){
 		listaAcessorios.add(item);
 	}
+	/**
+	 * Adiciona um carro ao plano de locacao
+	 * @param carro - Recebe um carro 
+	 * @throws Exception - Erro se o carro nao tiver os acessorios do plano
+	 */
 	public void adicionaCarro(Automovel carro)throws Exception{
 		int tamanho = listaAcessorios.size();
 		int acertos = 0;
@@ -31,7 +41,11 @@ public class PlanosAutomovel {
 		this.listaAuto.add(carro);
 		
 	}
-	
+	/**
+	 * Remove o carro do plano
+	 * @param carro - O carro a ser removido.
+	 * @throws Exception - Erro se o carro nao existe no cadastro do plano.
+	 */
 	public void removeCarro(Automovel carro) throws Exception{
 		if(!this.listaAuto.contains(carro)) {
 			throw new Exception("Esse modelo nao se encontra na lista do plano.");
@@ -39,11 +53,17 @@ public class PlanosAutomovel {
 			listaAuto.remove(carro);
 		}
 	}
-	
+	/**
+	 * Permite mudar o preco do plano
+	 * @param preco - O preço em double do plano
+	 */
 	public void setPreco(double preco){
 		this.preco = preco;
 	}
-	
+	/**
+	 * Captura o preco do plano
+	 * @return - Em double o preco
+	 */
 	public double getPreco(){
 		return this.preco;
 	}	
---  localisap2/Projeto P2 LOCALISA/src/planos/PlanosMoto.java
+++  localisap2/Projeto P2 LOCALISA/src/planos/PlanosMoto.java
@@ -10,20 +10,35 @@ public class PlanosMoto {
 	private List<Motocicleta> listaMoto = new ArrayList<Motocicleta>();
 	private int cilindradas;
 	private double preco;
-	
+	/**
+	 * Construtor de um plano de moto.
+	 * @param preco - Recebe o preco em double diario da locacao
+	 */	
 	public PlanosMoto(double preco){
 		this.preco = preco;
 	}
+	/**
+	 * Metodo que adiciona as cilindradas maxima do plano
+	 * @param cilindradas- Recebe como inteiro as cilindradas.
+	 */
 	public void adicionaCilindradas(int cilindradas){
 		this.cilindradas = cilindradas;
 	}
-	
+	/**
+	 * Adiciona uma moto ao plano de locacao
+	 * @param moto - Recebe uma moto
+	 * @throws Exception - Erro se a moto nao tiver as cilindradas do plano
+	 */
 	public void adicionaMoto(Motocicleta moto)throws Exception{
 		if (moto.getCilindradas()>cilindradas)
 			throw new Exception("A moto não atende as especicaçoes do plano");
 		this.listaMoto.add(moto);
 	}
-	
+	/**
+	 * Remove a moto do plano
+	 * @param carro - A motoo a ser removida.
+	 * @throws Exception - Erro se o carro nao existe no cadastro do plano.
+	 */
 	public void removeMoto(Motocicleta moto) throws Exception{
 		if(!this.listaMoto.contains(moto)) {
 			throw new Exception("Esse modelo nao se encontra na lista do plano.");
@@ -31,11 +46,17 @@ public class PlanosMoto {
 			listaMoto.remove(moto);
 		}
 	}
-	
+	/**
+	 * Permite mudar o preco do plano
+	 * @param preco - O preço em double do plano
+	 */
 	public void setPreco(double preco){
 		this.preco = preco;
 	}
-	
+	/**
+	 * Captura o preco do plano
+	 * @return - Em double o preco
+	 */
 	public double getPreco(){
 		return this.preco;
 	}
