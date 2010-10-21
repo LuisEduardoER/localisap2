dc72257590d2cf00960c156f16d06a295cf967e9
filipe.69 <filipe.69@91337772-dbf3-1cee-e463-7e92b6bf6caa>
2010-10-21 05:01:54 +0000

    Javadoc Agencias

---  localisap2/Projeto P2 LOCALISA/src/agencias/Agencia.java
+++  localisap2/Projeto P2 LOCALISA/src/agencias/Agencia.java
@@ -4,27 +4,54 @@ import funcionarios.Gerente;
 
 /**
  * 
- * @author Felipe Jos�
+ * @author Felipe Jose
  *
  */
 
 public interface Agencia {
+	/**
+	 * Metodo para capturar o CPNJ de uma agencia
+	 * @return - Em String o CPNJ
+	 */
 	String getCnpj();
 	String getEndereco();
+	/**
+	 * Metodo para capturar o telefone de uma agencia
+	 * @return - Em String o telefone
+	 */
 	String getTelefone();
+	/**
+	 * Metodo para capturar a inscricao estadual de uma agencia
+	 * @return - Em String a inscricao estadual
+	 */
 	String getInscEstadual();
+	/**
+	 * Metodo para capturar o gerente de uma agencia
+	 * @return - Em Gerente o gerente de uma agencia
+	 */
 	Gerente getGerenteResponsavel();
-	/*
-	void removeCNPJ();
-	void removeEndereco();
-	void removeInscEstadual();
-	void removeTelefone();
-	void removeGerenteResponsavel();
-	*/
-	
+
+	/**
+	 * Permite mudar o CPNJ
+	 * @param CPNJ - Recebe como String o CPNJ
+	 * @throws Exception - Erro de cpf invalido
+	 */	
 	void setCnpj(String cnpj) throws Exception;
+	
 	void setEndereco(String endereco)throws Exception;
+	/**
+	 * Permite mudar a inscricao estadual
+	 * @param inscricaoEstadual - Recebe como String a inscricao estadual
+	 */
 	void setInscEstadual(String inscEstadual)throws Exception;
+	/**
+	 * Permite mudar o telefone
+	 * @param telefone - Recebe como String o telefone
+	 */
 	void setTelefone(String telefone)throws Exception;
-	void setGerenteResponsavel(Gerente gerente)throws Exception;	
+	/**
+	 * Permite mudar o gerente responsavel
+	 * @param gerente - Recebe como Gerente o gerente responsavel
+	 */
+	void setGerenteResponsavel(Gerente gerente);	
 }
