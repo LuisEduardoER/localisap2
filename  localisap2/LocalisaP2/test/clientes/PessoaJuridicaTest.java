815cd64b61e6b401ac7833a61d6953b10dfa05c3
fjfcavalcanti <fjfcavalcanti@91337772-dbf3-1cee-e463-7e92b6bf6caa>
2010-10-22 23:48:45 +0000

    projeto adaptado para o netbeans, visando maior facilidade futura para criar a GUI.

---
+++  localisap2/LocalisaP2/test/clientes/PessoaJuridicaTest.java
@@ -0,0 +1,62 @@
+package clientes;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+*
+* @author Filipe Alencar   -twitter.com/filipealencar_
+* @author Felipe Jose      -twitter.com/felipejosefc
+* @author Emilio Farias    -twitter.com/militofarias
+*
+* http://code.google.com/p/localisap2/
+* Universidade Federal de Campina Grande - Computacao
+*
+*/
+
+public class PessoaJuridicaTest {
+	PessoaJuridica empresa;
+	Endereco endereco, endereco2;
+
+	@Before
+	public void inicio() throws Exception{
+		empresa = new PessoaJuridica();
+		empresa.setRazaoSocial("LocaLisa");
+		empresa.setCnpj("30851837000105");
+		empresa.setEmail("filipe@teste");
+		empresa.setEndereco(endereco);
+		empresa.setNomeFantasia("Fantasia");
+		empresa.setInscricaoEstadual("123456");
+		empresa.setTelefone("8333311111");
+	}
+
+	@Test
+	public void testaMetodosGet(){
+		Assert.assertEquals("30851837000105",empresa.getCnpj());
+		Assert.assertEquals("LocaLisa",empresa.getRazaoSocial());
+		Assert.assertEquals("filipe@teste",empresa.getEmail());
+		Assert.assertEquals(endereco,empresa.getEndereco());
+		Assert.assertEquals("Fantasia",empresa.getNomeFantasia());
+		Assert.assertEquals("123456",empresa.getInscricaoEstadual());
+		Assert.assertEquals("8333311111",empresa.getTelefone());
+
+	}
+	@Test
+	public void testaMetodosSet() throws Exception{
+		empresa.setRazaoSocial("agencia2");
+		empresa.setCnpj("75416229000100");
+		empresa.setEmail("filipe@teste2");
+		empresa.setEndereco(endereco2);
+		empresa.setNomeFantasia("Filipe A");
+		empresa.setInscricaoEstadual("1234567");
+		empresa.setTelefone("8333312222");
+		Assert.assertEquals("75416229000100",empresa.getCnpj());
+		Assert.assertEquals("agencia2",empresa.getRazaoSocial());
+		Assert.assertEquals("filipe@teste2",empresa.getEmail());
+		Assert.assertEquals(endereco2,empresa.getEndereco());
+		Assert.assertEquals("Filipe A",empresa.getNomeFantasia());
+		Assert.assertEquals("1234567",empresa.getInscricaoEstadual());
+		Assert.assertEquals("8333312222",empresa.getTelefone());
+	}
+}
---
+++  localisap2/LocalisaP2/test/funcionarios/LocadorTest.java
@@ -0,0 +1,79 @@
+package funcionarios;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import clientes.Endereco;
+
+import agencias.Agencia;
+import agencias.Filial;
+
+
+/**
+*
+* @author Filipe Alencar   -twitter.com/filipealencar_
+* @author Felipe Jose      -twitter.com/felipejosefc
+* @author Emilio Farias    -twitter.com/militofarias
+*
+* http://code.google.com/p/localisap2/
+* Universidade Federal de Campina Grande - Computacao
+*
+*/
+public class LocadorTest {
+	Locador locador;
+	Agencia filial1, filial2;
+	Gerente gerente;
+	Endereco endereco, endereco2;
+	@Before
+	public void inicio() throws Exception{
+		filial1 = new Filial("51851873000149", endereco, "8333331234", "01234", gerente);
+		filial2 = new Filial("58694458000178", endereco2, "8300004321", "56789", gerente);
+		locador = new Locador();
+		locador.setAgencia(filial1);
+		locador.setCpf("32456373570");
+		locador.setEmail("filipe@teste");
+		locador.setEndereco(endereco2);
+		locador.setNascimento("11/10/1980");
+		locador.setNaturalidade("Paraiba");
+		locador.setNome("Filipe");
+		locador.setRg("123456");
+		locador.setTelefone("8333311111");
+	}
+
+	@Test
+	public void testaMetodosGet(){
+		Assert.assertEquals("32456373570",locador.getCpf());
+		Assert.assertEquals(filial1,locador.getAgencia());
+		Assert.assertEquals("filipe@teste",locador.getEmail());
+		Assert.assertEquals(endereco2,locador.getEndereco());
+		Assert.assertEquals("11/10/1980",locador.getNascimento());
+		Assert.assertEquals("Paraiba",locador.getNaturalidade());
+		Assert.assertEquals("Filipe",locador.getNome());
+		Assert.assertEquals("123456",locador.getRg());
+		Assert.assertEquals("8333311111",locador.getTelefone());
+
+	}
+
+	@Test
+	public void testaMetodosSet() throws Exception{
+		locador.setAgencia(filial2);
+		locador.setCpf("58548316780");
+		locador.setEmail("filipe@teste2");
+		locador.setEndereco(endereco);
+		locador.setNascimento("10/10/1980");
+		locador.setNaturalidade("Sao Paulo");
+		locador.setNome("Filipe A");
+		locador.setRg("1234567");
+		locador.setTelefone("8333312222");
+		Assert.assertEquals("58548316780",locador.getCpf());
+		Assert.assertEquals(filial2,locador.getAgencia());
+		Assert.assertEquals("filipe@teste2",locador.getEmail());
+		Assert.assertEquals(endereco,locador.getEndereco());
+		Assert.assertEquals("10/10/1980",locador.getNascimento());
+		Assert.assertEquals("Sao Paulo",locador.getNaturalidade());
+		Assert.assertEquals("Filipe A",locador.getNome());
+		Assert.assertEquals("1234567",locador.getRg());
+		Assert.assertEquals("8333312222",locador.getTelefone());
+	}
+}
