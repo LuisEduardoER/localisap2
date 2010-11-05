e27ceec65ef1f68c0321ef318c1835dbe2206644
filipe.69 <filipe.69@91337772-dbf3-1cee-e463-7e92b6bf6caa>
2010-11-05 03:11:07 +0000

    Refatoramento de Pessoas , criando uma classe abstrata

--- trunk/ localisap2/LocalisaP2/src/funcionarios/Gerente.java
+++ trunk/ localisap2/LocalisaP2/src/funcionarios/Gerente.java
@@ -18,16 +18,9 @@ import verificacoes.Validacao;
 * Universidade Federal de Campina Grande - Computacao
 *
 */
-public class Gerente implements Pessoa, Serializable{
-	private String cpf;
-	private String nome;
-	private String rg;
-	private String nascimento;
-	private String naturalidade;
-	private Endereco endereco;
+public class Gerente extends PessoaAbstrata implements Serializable{
+
 	private Filial agencia;
-	private String telefone;
-	private String email;
         private String codigoDoGerente;
 	/**
 	 * Construtor que cria um gerente.
@@ -62,99 +55,8 @@ public class Gerente implements Pessoa, Serializable{
         public String getCodigoExclusivo(){
             return codigoDoGerente;
         }
-	@Override
-	public String getCpf() {
-		return cpf;
-	}
-
-	@Override
-	public String getNome() {
-		return nome;
-	}
-
-	@Override
-	public String getRg() {
-		return rg;
-	}
-
-	@Override
-	public String getNascimento() {
-		return nascimento;
-	}
-
-	@Override
-	public String getNaturalidade() {
-		return naturalidade;
-	}
-
-	@Override
-	public Endereco getEndereco() {
-		return endereco;
-	}
-
-	/**
-	 * Captura a agencia
-	 * @return - Como Filial a agencia
-	 */
 	public Filial getAgencia() {
 		return agencia;
-	}
-
-	@Override
-	public String getTelefone() {
-		return telefone;
-	}
-
-	@Override
-	public String getEmail() {
-		return email;
-	}
-
-	@Override
-	public void setCpf(String cpf) throws Exception{
-		Validacao testeCpf = new Validacao();
-		if(!(testeCpf.validaCpf(cpf)))
-			throw new Exception("CPF invalido.");
-		this.cpf = cpf;
-
-	}
-
-	@Override
-	public void setNome(String nome) throws Exception{
-		if(nome.length()==0 )
-			throw new Exception("O nome nao deve ser vazio");
-		this.nome = nome;
-
-	}
-
-	@Override
-	public void setRg(String rg) throws Exception {
-		if(rg.length()==0)
-			throw new Exception("O RG nao deve ser vazio");
-		this.rg = rg;
-	}
-
-	@Override
-	public void setNascimento(String nascimento) throws Exception{
-		Validacao testaData = new Validacao();
-		if(!(testaData.validaData(nascimento)))
-			throw new Exception("Data invalida");
-		this.nascimento = nascimento;
-
-	}
-
-	@Override
-	public void setNaturalidade(String naturalidade) throws Exception{
-		if(naturalidade.length()==0)
-			throw new Exception("A naturalidade nao deve ser vazia");
-		this.naturalidade = naturalidade;
-
-	}
-
-	@Override
-	public void setEndereco(Endereco endereco) throws Exception{
-		this.endereco = endereco;
-
 	}
 
 	/**
@@ -165,21 +67,4 @@ public class Gerente implements Pessoa, Serializable{
 		this.agencia = agencia;
 
 	}
-
-	@Override
-	public void setTelefone(String telefone)throws Exception {
-		if(telefone.length()!= 10)
-			throw new Exception("Um telefone valido deve ter o DD + o numero");
-		this.telefone = telefone;
-
-	}
-
-	@Override
-	public void setEmail(String email) throws Exception{
-		if (!email.contains("@") || email.length()==0)
-			throw new Exception("O email deve conter @ e ser maior que 3");
-
-		this.email = email;
-	}
-
 }
--- trunk/ localisap2/LocalisaP2/src/funcionarios/Locador.java
+++ trunk/ localisap2/LocalisaP2/src/funcionarios/Locador.java
@@ -18,7 +18,7 @@ import verificacoes.Validacao;
 * Universidade Federal de Campina Grande - Computacao
 *
 */
-public class Locador implements Pessoa, Serializable{
+public class Locador extends PessoaAbstrata implements Serializable{
 	private String cpf;
 	private String nome;
 	private String rg;
@@ -63,35 +63,7 @@ public class Locador implements Pessoa, Serializable{
         public String getCodigoExclusivo(){
             return codigoDoLocador;
         }
-	@Override
-	public String getCpf() {
-		return cpf;
-	}
 
-	@Override
-	public String getNome() {
-		return nome;
-	}
-
-	@Override
-	public String getRg() {
-		return rg;
-	}
-
-	@Override
-	public String getNascimento() {
-		return nascimento;
-	}
-
-	@Override
-	public String getNaturalidade() {
-		return naturalidade;
-	}
-
-	@Override
-	public Endereco getEndereco() {
-		return endereco;
-	}
 
 	/**
 	 * Captura a agencia
@@ -101,63 +73,6 @@ public class Locador implements Pessoa, Serializable{
 		return agencia;
 	}
 
-	@Override
-	public String getTelefone() {
-		return telefone;
-	}
-
-	@Override
-	public String getEmail() {
-		return email;
-	}
-
-	@Override
-	public void setCpf(String cpf) throws Exception{
-		Validacao testeCpf = new Validacao();
-		if(!(testeCpf.validaCpf(cpf)))
-			throw new Exception("CPF invalido.");
-		this.cpf = cpf;
-
-	}
-
-	@Override
-	public void setNome(String nome) throws Exception{
-		if(nome.length()==0 )
-			throw new Exception("O nome nao deve ser vazio");
-		this.nome = nome;
-
-	}
-
-	@Override
-	public void setRg(String rg) throws Exception {
-		if(rg.length()==0)
-			throw new Exception("O RG nao deve ser vazio");
-		this.rg = rg;
-	}
-
-
-	@Override
-	public void setNascimento(String nascimento) throws Exception{
-		Validacao testaData = new Validacao();
-		if(!(testaData.validaData(nascimento)))
-			throw new Exception("Data invalida");
-		this.nascimento = nascimento;
-
-	}
-
-	@Override
-	public void setNaturalidade(String naturalidade) throws Exception{
-		if(naturalidade.length()==0)
-			throw new Exception("A naturalidade nao deve ser vazia");
-		this.naturalidade = naturalidade;
-
-	}
-
-	@Override
-	public void setEndereco(Endereco endereco) throws Exception{
-		this.endereco = endereco;
-
-	}
 
 	/**
 	 * Permite mudar a agencia.
@@ -168,19 +83,5 @@ public class Locador implements Pessoa, Serializable{
 
 	}
 
-	@Override
-	public void setTelefone(String telefone)throws Exception {
-		if(telefone.length()!= 10)
-			throw new Exception("Um telefone valido deve ter o DD + o numero");
-		this.telefone = telefone;
 
-	}
-
-	@Override
-	public void setEmail(String email) throws Exception{
-		if (!email.contains("@")||email.length()==0)
-			throw new Exception("O email deve conter @ e ser maior que 3");
-
-		this.email = email;
-	}
 }
