7a7fb55f11e9ef2a0360ba1e3609dcbd97a4ec1b
filipe.69 <filipe.69@91337772-dbf3-1cee-e463-7e92b6bf6caa>
2010-11-05 03:11:07 +0000

    Refatoramento de Pessoas , criando uma classe abstrata

---  localisap2/LocalisaP2/src/clientes/PessoaFisica.java
+++  localisap2/LocalisaP2/src/clientes/PessoaFisica.java
@@ -1,6 +1,7 @@
 package clientes;
 
 import funcionarios.Pessoa;
+import funcionarios.PessoaAbstrata;
 import java.io.Serializable;
 import verificacoes.Validacao;
 
@@ -14,7 +15,7 @@ import verificacoes.Validacao;
 * Universidade Federal de Campina Grande - Computacao
 *
 */
-public class PessoaFisica implements Pessoa,Serializable{
+public class PessoaFisica extends PessoaAbstrata implements Serializable{
 	private String cpf;
 	private String nome;
 	private String rg;
@@ -55,152 +56,5 @@ public class PessoaFisica implements Pessoa,Serializable{
         public String getCodigoExclusivo(){
             return codigoDaPessoaFisica;
         }
-	@Override
-	public String getCpf() {
-		return cpf;
-	}
-
-	@Override
-	public String getNome() {
-		return nome;
-	}
-
-	@Override
-	public String getRg() {
-		return rg;
-	}
-
-	@Override
-	public String getNascimento() {
-		return nascimento;
-	}
-
-	@Override
-	public String getNaturalidade() {
-		return naturalidade;
-	}
-
-	@Override
-	public Endereco getEndereco() {
-		return endereco;
-	}
-
-
-	@Override
-	public String getTelefone() {
-		return telefone;
-	}
-
-	@Override
-	public String getEmail() {
-		return email;
-	}
-/*
-	@Override
-	public void removeCpf() {
-		cpf = null;
-	}
-
-	@Override
-	public void removeNome() {
-		nome = null;
-	}
-
-	@Override
-	public void removeRG() {
-		rg = null;
-	}
-
-	@Override
-	public void removeNascimento() {
-		nascimento = null;
-	}
-
-	@Override
-	public void removeNaturalidade() {
-		naturalidade = null;
-	}
-
-	@Override
-	public void removeEndereco() {
-		endereco = null;
-	}
-
-	@Override
-	public void removeAgencia() {
-		agencia = null;
-	}
-
-	@Override
-	public void removeTelefone() {
-		telefone = null;
-	}
-
-	@Override
-	public void removeEmail() {
-		email = null;
-	}
-*/
-	@Override
-	public void setCpf(String cpf) throws Exception{
-		Validacao testeCpf = new Validacao();
-		if(!(testeCpf.validaCpf(cpf)))
-			throw new Exception("CPF invalido.");
-		this.cpf = cpf;
-
-	}
-
-	@Override
-	public void setNome(String nome) throws Exception{
-		if(nome.length()==0 )
-			throw new Exception("O nome nao deve ser vazio");
-		this.nome = nome;
-
-	}
-
-	@Override
-	public void setRg(String rg) throws Exception {
-		if(rg.length()==0)
-			throw new Exception("O RG nao deve ser vazio");
-		this.rg = rg;
-	}
-
-	@Override
-	public void setNascimento(String nascimento) throws Exception{
-		Validacao testaData = new Validacao();
-		if(!(testaData.validaData(nascimento)))
-			throw new Exception("Data invalida");
-		this.nascimento = nascimento;
-
-	}
-
-	@Override
-	public void setNaturalidade(String naturalidade) throws Exception{
-		if(naturalidade.length()==0)
-			throw new Exception("A naturalidade nao deve ser vazia");
-		this.naturalidade = naturalidade;
-
-	}
-
-	@Override
-	public void setEndereco(Endereco endereco){
-		this.endereco = endereco;
-	}
-
-
-	@Override
-	public void setTelefone(String telefone)throws Exception {
-		if(telefone.length()!= 10)
-			throw new Exception("Um telefone valido deve ter o DD + o numero");
-		this.telefone = telefone;
-
-	}
-
-	@Override
-	public void setEmail(String email) throws Exception{
-		if (!email.contains("@")||email.length()==0)
-			throw new Exception("O email deve conter @ e ser maior que 3");
-
-		this.email = email;
-	}
+	
 }
---
+++  localisap2/LocalisaP2/src/funcionarios/PessoaAbstrata.java
@@ -0,0 +1,131 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package funcionarios;
+import Funcoes.verificacoes.Validacao;
+import clientes.Endereco;
+import java.io.Serializable;
+
+
+/**
+ *
+ * @author Filipe
+ */
+public abstract class PessoaAbstrata implements Pessoa,Serializable{
+        private String cpf;
+	private String nome;
+	private String rg;
+	private String nascimento;
+	private String naturalidade;
+	private Endereco endereco;
+	private String telefone;
+	private String email;
+
+
+	@Override
+	public String getCpf() {
+		return cpf;
+	}
+
+	@Override
+	public String getNome() {
+		return nome;
+	}
+
+	@Override
+	public String getRg() {
+		return rg;
+	}
+
+	@Override
+	public String getNascimento() {
+		return nascimento;
+	}
+
+	@Override
+	public String getNaturalidade() {
+		return naturalidade;
+	}
+
+	@Override
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	@Override
+	public String getTelefone() {
+		return telefone;
+	}
+
+	@Override
+	public String getEmail() {
+		return email;
+	}
+
+	@Override
+	public void setCpf(String cpf) throws Exception{
+		Validacao testeCpf = new Validacao();
+		if(!(testeCpf.validaCpf(cpf)))
+			throw new Exception("CPF invalido.");
+		this.cpf = cpf;
+
+	}
+
+	@Override
+	public void setNome(String nome) throws Exception{
+		if(nome.length()==0 )
+			throw new Exception("O nome nao deve ser vazio");
+		this.nome = nome;
+
+	}
+
+	@Override
+	public void setRg(String rg) throws Exception {
+		if(rg.length()==0)
+			throw new Exception("O RG nao deve ser vazio");
+		this.rg = rg;
+	}
+
+	@Override
+	public void setNascimento(String nascimento) throws Exception{
+		Validacao testaData = new Validacao();
+		if(!(testaData.validaData(nascimento)))
+			throw new Exception("Data invalida");
+		this.nascimento = nascimento;
+
+	}
+
+	@Override
+	public void setNaturalidade(String naturalidade) throws Exception{
+		if(naturalidade.length()==0)
+			throw new Exception("A naturalidade nao deve ser vazia");
+		this.naturalidade = naturalidade;
+
+	}
+
+	@Override
+	public void setEndereco(Endereco endereco) throws Exception{
+		this.endereco = endereco;
+
+	}
+
+	@Override
+	public void setTelefone(String telefone)throws Exception {
+		if(telefone.length()!= 10)
+			throw new Exception("Um telefone valido deve ter o DDD + o numero");
+		this.telefone = telefone;
+
+	}
+
+	@Override
+	public void setEmail(String email) throws Exception{
+		if (!email.contains("@") || email.length()==0)
+			throw new Exception("O email deve conter @ e ser maior que 3");
+
+		this.email = email;
+	}
+
+
+}
