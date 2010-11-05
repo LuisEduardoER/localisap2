455c637b082ad480055bea69e39d538e02442967
militoufcg <militoufcg@91337772-dbf3-1cee-e463-7e92b6bf6caa>
2010-11-05 03:44:56 +0000

    testes de erros no teste da Filial

---  localisap2/LocalisaP2/test/agencias/FilialTest.java
+++  localisap2/LocalisaP2/test/agencias/FilialTest.java
@@ -44,14 +44,32 @@ public class FilialTest {
 
 	@Test
 	public void testaMetodosSet() throws Exception{
-/*		try {
+		try {
 			filial1.setCnpj("1234");
+                        Assert.fail("Esperava excecao de setCnpj");
 		} catch (Exception ex){
-			ex.getMessage();
-		}*/
+			Assert.assertEquals("erro", "Um cnpj valido deve ter 14 numeros", ex.getMessage());
+		}
+
 		filial1.setCnpj("58694458000178");
 		filial1.setEndereco(endereco2);
+
+                try {
+                    filial1.setTelefone("123");
+                    Assert.fail("Excecao de setTelefone esperada.");
+                } catch (Exception ex){
+                    Assert.assertEquals("erro", "Um telefone valido deve ter o DD + o numero", ex.getMessage());
+                }
+
 		filial1.setTelefone("8300004321");
+
+                try {
+                    filial1.setInscEstadual("");
+                    Assert.fail("Excecao de setInscEstadual esperada");
+                } catch (Exception ex) {
+                    Assert.assertEquals("erro", "A inscricao estadual nao deve ser vazio", ex.getMessage());
+                }
+
 		filial1.setInscEstadual("56789");
 
 		Assert.assertEquals("58694458000178", filial1.getCnpj());
